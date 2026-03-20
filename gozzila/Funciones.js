7cb0489f0c5d30d321f81b0249a3353ba5b76fdd
Davinson_C <Davinsonchz@gmail.com>
2026-03-20 15:52:43 -0500

    funciones calcular fecha

---
+++ gozzila/Funciones.js
@@ -0,0 +1,17 @@
+
+
+function calcular_Edad(){
+  return calcular_Edad
+ 
+
+}
+
+p=parseInt(prompt("ingrese su fecha de nacimiento"))
+calcular_Edad(n-p)
+const add = (a, b) => {
+  return a + b;
+};
+
+a = parseInt(prompt("ingrese Numero 1"))
+b = parseInt(prompt("ingrese Numero 2"))
+console.log("la suma de " + a,"+",b,"es",add(a,b))
